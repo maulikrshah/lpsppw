386ae3d355d88d5f224c870fb8d439954f5c0baa
maulikrshah <maulik.r.shah@verizon.net>
2018-02-19 21:20:23 -0500

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // include http module in the file
 var http = require('http');
-var mysql = require('mysql');
+//var mysql = require('mysql');
+//var mongo = require('mongodb');
+
+//var MongoClient = require('mongodb').MongoClient;
+//var url = "mongodb://localhost:27017/mydb";
+
+//MongoClient.connect(url, function(err, db) {
+  //if (err) throw err;
+  //console.log("Database created!");
+  //db.close();
+//});
 
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
