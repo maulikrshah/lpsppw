063d98289b64d7329e300ca410b279897bc396a7
maulikrshah <maulik.r.shah@verizon.net>
2018-03-21 16:30:47 -0400

    test email

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 // include http module in the file
 
 var http = require('http');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'maulik.r.shah29@gmail.com',
+		pass: 'mandm2915'
+	      }
+});
+
+var mailOptions = {
+	from: 'maulik.r.shah29@gmail.com',
+	to: 'lpsminimart@verizon.net',
+	subject: 'Sending test email from server',
+	text: 'Testing 3.21.838'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+	if(error){
+	console.log(error);
+	} else {
+	console.log('Email sent: ' + info.response);
+	}
+});
 
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
